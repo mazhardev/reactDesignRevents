914235f0076116403d15e6e830d7641beca1ad05
Muhammad Mazhar <37449942+mazhardev@users.noreply.github.com>
2019-03-28 23:30:47 +0500

    Home Page design

---
+++ src/app/layout/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+import Home from "../../features/home/Home";
+class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
+
+export default App;
